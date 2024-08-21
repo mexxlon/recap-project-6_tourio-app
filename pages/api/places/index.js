f3023b825490f440ab3b3bb081ec922794238a04
Maximilian Hammes <maximview@gmail.com>
2024-08-21 11:51:24 +0200

    add create form

--- pages/api/places/index.js
+++ pages/api/places/index.js
@@ -5,10 +5,22 @@ export default async function handler(request, response) {
   await dbConnect();
 
   if (request.method === "GET") {
-    const place = await Place.find({});
+    const places = await Place.find({});
+    if (places) {
+      response.status(200).json(places);
+    } else {
+      response.status(500).json({ message: "Internal Server Error" });
+    }
+  } else if (request.method === "POST") {
+    const place = new Place(request.body);
+    const savedPlace = await place.save();
 
-    response.status(200).json(place);
+    if (savedPlace) {
+      response.status(201).json(savedPlace);
+    } else {
+      response.status(400).json({ message: "Bad Request" });
+    }
   } else {
-    response.status(405).json({ message: "Failed" });
+    response.status(405).json({ message: "Method Not Allowed" });
   }
 }
